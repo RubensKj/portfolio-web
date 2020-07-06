1e383f8466090421e5eb2dab48563c8bf7dffb32
Rubens Kleinschmidt Júnior <36218162+RubensKj@users.noreply.github.com>
2020-07-06 19:57:43 -0300

    Adding effects to pages

--- src/pages/Certifications/index.tsx
+++ src/pages/Certifications/index.tsx
@@ -4,34 +4,44 @@ import React, { useState, useEffect } from 'react';
 import api from '../../services/api';
 
 // Components
+import LoadingPage from '../../components/LoadingPage';
 import TransitionText from '../../components/TransitionText';
 import ListCertifications from '../../components/ListCertifications';
+
+// Interfaces
 import { Certification } from '../../components/CertificationCard';
 
 import { Container } from './styles';
 
 const Certifications: React.FC = () => {
+  const [isLoading, setIsLoading] = useState<boolean>(false);
   const [certifications, setCertifications] = useState<Certification[]>([]);
 
   useEffect(() => {
+    setIsLoading(true);
     async function getCertifications(): Promise<void> {
       const response = await api.get('certifications');
 
       setCertifications(response.data);
+      setIsLoading(false);
     }
 
     getCertifications();
   }, []);
 
   return (
-    <Container>
-      <TransitionText
-        title="Here are all my certifications"
-        description="These are my certifications that I got until now, more are coming (And projects are too :))"
-        marginTop={0}
-      />
-      <ListCertifications list={certifications} />
-    </Container>
+    <>
+      {isLoading ? <LoadingPage /> : (
+        <Container>
+          <TransitionText
+            title="Here are all my certifications"
+            description="These are my certifications that I got until now, more are coming (And projects are too :))"
+            marginTop={0}
+          />
+          <ListCertifications list={certifications} />
+        </Container>
+      )}
+    </>
   );
 }
 
--- src/pages/Projects/index.tsx
+++ src/pages/Projects/index.tsx
@@ -1,20 +1,51 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
+// Services
+import apiGithub from '../../services/apiGithub';
+
+// Components
+import LoadingPage from '../../components/LoadingPage';
 import TransitionText from '../../components/TransitionText';
 import ListProjects from '../../components/ListProjects';
+
+// Interfaces
+import { Project } from '../../components/ProjectCard';
 
 import { Container } from './styles';
 
 const Projects: React.FC = () => {
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [projects, setProjects] = useState<Project[]>([]);
+
+  useEffect(() => {
+    setIsLoading(true);
+    async function getProjects(): Promise<void> {
+      await apiGithub.get('/users/RubensKj/repos')
+        .then(data => {
+          setProjects(data.data)
+          setIsLoading(false);
+        })
+        .catch(error => {
+          console.log(error);
+        });
+    }
+
+    getProjects();
+  }, []);
+
   return (
-    <Container>
-      <TransitionText
-        title="Here are all my projects"
-        description="These are my projects that I made until now, more are coming!!"
-        marginTop={0}
-      />
-      <ListProjects list={[]} />
-    </Container>
+    <>
+      {isLoading ? <LoadingPage /> : (
+        <Container>
+          <TransitionText
+            title="Here are all my projects"
+            description="These are my projects that I made until now, more are coming!!"
+            marginTop={0}
+          />
+          <ListProjects list={projects} />
+        </Container>
+      )}
+    </>
   );
 }
 

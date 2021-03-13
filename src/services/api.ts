6ee686e3348153c86610da0a009e9c677245b60f
rubenskj <rubenskleinschmidtjr@gmail.com>
2021-03-13 15:52:16 -0300

    Adding sufix

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || 'http://localhost:8080',
+  baseURL: process.env.REACT_APP_API_URL || 'http://localhost:8080/api',
 })
 
 export default api;
